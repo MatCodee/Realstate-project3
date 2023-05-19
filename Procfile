0e39d55647056a9159730bd4dbda9d7b3b748763
Matish1 <matias.altamiranove@gmail.com>
2023-05-19 14:10:55 -0400

    repair template

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn 'app.wsgi'
+web: python manage.py migrate && gunicorn app.wsgi --log-file -
--- app/settings.py
+++ app/settings.py
@@ -74,9 +74,9 @@ SITEMAP_TEMPLATE = 'sitemap.xml'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR,'templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
-        'OPTIONS': {
+        'OPTIONS': {    
             'context_processors': [
                 'django.template.context_processors.debug',
                 'django.template.context_processors.request',
