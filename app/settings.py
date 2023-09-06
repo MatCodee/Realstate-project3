0e261044512bca65ce863545ab1f234f78fbf767
Matias <matias.altamiranove@gmail.com>
2023-09-06 17:57:45 -0300

    add doc

--- app/settings.py
+++ app/settings.py
@@ -30,8 +30,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-#DEBUG = True
-DEBUG = env.bool('DEBUG', default=False)
+DEBUG = True
+#DEBUG = env.bool('DEBUG', default=False)
 
 ALLOWED_HOSTS = ["*"]
 
